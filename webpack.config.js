859a8a4f9af5a499b3b30a0c74a8e339f24b7bd5
Neal Stewart <nes@tradeshift.com>
2016-10-23 15:22:21 +0200

    Random BS

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+	devtool: 'source-map'
+};
